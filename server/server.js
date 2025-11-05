538c0c5b493c4655563ca7f29fea12b769902cf7
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-05 11:54:06 -0500

    Add better-auth's route handler

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 import cors from 'cors';
 
 const corsOptions = {
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173"],
+    credentials: true
 };
 
 const app = express();
 app.use(cors(corsOptions));
 const PORT = 5000
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json())
 
 app.get('/api', (req, res) => {
     res.json({"users": ["Jaime", "Sara", "Prada"]})
