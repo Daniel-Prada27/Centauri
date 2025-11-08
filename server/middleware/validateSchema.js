510a82205a453c4532c4130762d898ecbbaf4e30
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-08 13:37:32 -0500

    Add team schema validation

--- server/middleware/validateSchema.js
+++ server/middleware/validateSchema.js
@@ -1,8 +1,18 @@
 import { UserProfileSchema } from '#server/models/schema.user_profile.js';
+import { TeamSchema } from '#server/models/schema.team.js'
 
 export const validateUserProfile = (req, res, next) => {
     try {
         UserProfileSchema.parse(req.body);
+        next();
+    } catch (error) {
+        return res.status(400).json({ message: 'Invalid request body', details: error.errors });
+    }
+};
+
+export const validateTeam = (req, res, next) => {
+    try {
+        TeamSchema.parse(req.body);
         next();
     } catch (error) {
         return res.status(400).json({ message: 'Invalid request body', details: error.errors });
