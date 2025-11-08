17d6c0d11b0d05c16a024abe8a057501b47d03bf
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-08 13:37:57 -0500

    Create team controller file with create handler

---
+++ server/controllers/team.controller.js
@@ -0,0 +1,21 @@
+import * as teamService from '#server/services/team.service.js'
+import { TeamSchema } from '#server/models/schema.team.js'
+import { authClient } from '#server/lib/auth-client.js'
+
+
+export const createTeam = async (req, res) => {
+    try {
+
+        const team = TeamSchema.parse(req.body)
+        const userId = req.session.user.id
+
+        const result = await teamService.createTeam(userId, team)
+        res.status(201).json(result)
+    } catch (error) {
+        if (error.statusCode) {
+            return res.status(error.statusCode).json({ message: error.message });
+        }
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong!' });
+    }
+}
