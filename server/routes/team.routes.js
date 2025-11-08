dfe831dbeec0854251098ff80b01ee6698ec24f6
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-08 13:38:36 -0500

    Add team creation route handler

---
+++ server/routes/team.routes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import * as controller from '#server/controllers/team.controller.js'
+import * as SchemaValidation from '#server/middleware/validateSchema.js'
+import * as ValidateSession from '#server/middleware/validateSession.js'
+
+export const teamRouter = express.Router();
+
+teamRouter.post('/create-team',
+    ValidateSession.validateSession,
+    SchemaValidation.validateTeam,
+    controller.createTeam);
