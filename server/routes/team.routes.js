37dff541fb77375728b1871a0c2564c326bcec55
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-09 01:13:05 -0500

    Add example routes

--- server/routes/team.routes.js
+++ server/routes/team.routes.js
@@ -5,20 +5,20 @@ import * as ValidateSession from '#server/middleware/validateSession.js'
 
 export const teamRouter = express.Router();
 
-teamRouter.post('',
+teamRouter.post('', // POST http://localhost:3000/team
     ValidateSession.validateSession,
     SchemaValidation.validateTeam,
     controller.createTeam);
 
-teamRouter.get('/get-teams',
+teamRouter.get('/get-teams', // GET http://localhost:3000/team/get-teams
     ValidateSession.validateSession,
     controller.readTeam);
 
-teamRouter.put('/:teamId',
+teamRouter.put('/:teamId', // PUT http://localhost:3000/team/41235436-f779-4e30-j803-648945736b52
     ValidateSession.validateSession,
     controller.updateTeam);
 
-teamRouter.delete("/:teamId",
+teamRouter.delete("/:teamId", // DELETE http://localhost:3000/team/47845436-p079-4e30-b603-648945736b52
     ValidateSession.validateSession,
     controller.deleteTeam
 )
