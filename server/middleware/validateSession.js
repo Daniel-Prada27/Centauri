5a4bba2d6b5072d2be2cd826abfdab470656171f
sarapg25 <sara2221923@correo.uis.edu.co>
2025-11-24 00:03:17 -0500

    Se cambio en el backend el validateSession

--- server/middleware/validateSession.js
+++ server/middleware/validateSession.js
@@ -6,12 +6,16 @@ export const validateSession = async (req, res, next) => {
             headers: req.headers
         });
 
-        if (session) {
-            req.session = session;
-            next();
-        } else {
+        if (!session || !session.user) {
             return res.status(401).json({ message: "No valid session found." });
         }
+
+        req.session = session;
+        req.user_id = session.user.id;     
+        req.user = session.user;           
+
+        next();
+        
     } catch (error) {
         console.error("Error retrieving session:", error);
         return res.status(500).json({ message: "Server error", error: error.message });
